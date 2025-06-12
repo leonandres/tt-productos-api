3bf13794bb66c63a2b33f26318c917a771c9768e
leonandres <andresleonb90@gmail.com>
2025-06-12 03:06:18 -0300

    Se agrega get y post de producto sin llamado a api y se agrega dependencia para fetch

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 import {argv} from 'node:process';
+
+if (argv[2] == "GET" && argv[3] == "productos") {
+    console.log("GET, aún no llama a nada");
+}
+
+if(argv [2] == "POST" && argv[3] == "productos") {
+    console.log("POST, aún no llama a nada");
+}
+
+console.log("Argumentos recibidos:", {
+    parametro1: argv[0],
+    parametro2: argv[1],
+    método: argv[2],
+    recurso: argv[3],
+    otros: argv.slice(4)
+});
