59005d30c0fbaf0405a34d5e41ead4e1c3a8f8d3
leonandres <andresleonb90@gmail.com>
2025-06-12 03:34:15 -0300

    Se agrega llamado a la api, get productos

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 import {argv} from 'node:process';
+//Get
+if (argv[2] == "GET" && argv[3] == "products") {
+    const response = await fetch('http://fakestoreapi.com/products');
+    const data = await response.json();
+    console.log("GET, productos:", data);
+}
 
-if (argv[2] == "GET" && argv[3] == "productos") {
+if (argv[2] == "GET" && argv[3].startsWith("products/")) {
     console.log("GET, aún no llama a nada");
 }
 
-if(argv [2] == "POST" && argv[3] == "productos") {
+if(argv [2] == "POST" && argv[3] == "products") {
     console.log("POST, aún no llama a nada");
 }
+
+if (argv[2] == "POST" && argv[3].startsWith("products/")) {
+    console.log("POST, aún no llama a nada");
+}
+
+
 
 console.log("Argumentos recibidos:", {
     parametro1: argv[0],
